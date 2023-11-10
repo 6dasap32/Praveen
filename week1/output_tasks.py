f8444fbbb03dc3bb895cd9fc264387c3ebf2a37f
Praveen Navodya Jayawardana Dasanayaka Liyanage <6dasap32@solent.ac.uk>
2023-11-10 12:45:03 +0000

    Added code to display a simple message

--- week1/output_tasks.py
+++ week1/output_tasks.py
@@ -2,3 +2,6 @@
 # Display message to the screen
 print("Welcome to COM411!")
 print("In week 1 we will learn...")
+print("how to use git and github")
+print("how to output to the screen")
+print("how to get user input")
